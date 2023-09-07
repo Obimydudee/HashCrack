e03aff1a3351baf20e86fcd50dd940ee8484f8ce
Obi1 <obi@vnight-studios.xyz>
2023-09-07 20:51:23 +0200

    Add files via upload

---
+++ HashCrack/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace HashCrack
+{
+    internal static class Program
+    {
+        static List<string> ReadAllLinesFromTxt(string filename)
+        {
+            List<string> lines = new List<string>();
+
+            try
+            {
+                using (StreamReader strd = new StreamReader(filename))
+                {
+                    string line;
+                    while ((line = strd.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Error Reading");
+            }
+
+            return lines;
+        }
+
+
+        private static string[] StartString = new string[]
+        {
+            @"$$\   $$\                     $$\        $$$$$$\                               $$\       ",
+            @"$$ |  $$ |                    $$ |      $$  __$$\                              $$ |      ",
+            @"$$ |  $$ | $$$$$$\   $$$$$$$\ $$$$$$$\  $$ /  \__| $$$$$$\  $$$$$$\   $$$$$$$\ $$ |  $$\ ",
+            @"$$$$$$$$ | \____$$\ $$  _____|$$  __$$\ $$ |      $$  __$$\ \____$$\ $$  _____|$$ | $$  |",
+            @"$$  __$$ | $$$$$$$ |\$$$$$$\  $$ |  $$ |$$ |      $$ |  \__|$$$$$$$ |$$ /      $$$$$$  / ",
+            @"$$ |  $$ |$$  __$$ | \____$$\ $$ |  $$ |$$ |  $$\ $$ |     $$  __$$ |$$ |      $$  _$$<  ",
+            @"$$ |  $$ |\$$$$$$$ |$$$$$$$  |$$ |  $$ |\$$$$$$  |$$ |     \$$$$$$$ |\$$$$$$$\ $$ | \$$\ ",
+            @"\__|  \__| \_______|\_______/ \__|  \__| \______/ \__|      \_______| \_______|\__|  \__|",
+            @"                                                            //Made by Obi/ProxNet/ProxFox",
+            @"                                                                                         ",
+            @"                                                                                         "
+        };
+
+        private static string[] HelpString = new string[]
+        {
+            @"/ˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉ\",
+            @"|                                       USAGE:                                          |",
+            @"|     .\HashCrack.exe [path to passlist] [the path to the hashedPass] {-mLC | optional} |",
+            @"|                                                                                       |",
+            @"|                    optional end args= '-mLC' - '--multiLineCrack'                     |",
+            @"|                                                                                       |",
+            @"|                 Used when you want to crack multible hashes at once.                  |",
+            @"|                                                                                       |",
+            @"|                                 Contact Me:                                           |",
+            @"|                                                                                       |",
+            @"|                       Email - obi@vnight-studios.xyz                                  |",
+            @"|                       Github - https://github.com/Obimydudee                          |",
+            @"|                       Twitter - https://twitter.com/obimydude                         |",
+            @"|                       Bluesky - https://bsky.app/profile/veloxservers.lol             |",
+            @"|                                                                                       |",
+            @"\_______________________________________________________________________________________/",
+            @"                                                                                         "
+        };
+
+        static void Main(string[] args)
+        {
+            
+            if (args.Length == 0) {
+                Console.WriteLine("usage: [path to passlist] [the path to the hashedPass]");
+            }
+
+            string[] cmdLineArgs = Environment.GetCommandLineArgs();
+
+            if (cmdLineArgs.Contains("-h") || cmdLineArgs.Contains("--help"))
+            {
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                for (int i = 0; i < 17; i++) //10 meaning the amount of lines the string[] logo has
+                {
+                    Console.WriteLine(HelpString[i]);
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Clear();
+                string wordlist = cmdLineArgs[1];
+                string hashpath = cmdLineArgs[2];
+                string HashToCrack = null;
+                List<string> HashesToCrack = new List<string>();
+                List<string> HashLine = ReadAllLinesFromTxt(hashpath);
+
+
+
+
+
+                if (cmdLineArgs.Contains("-mLC") || cmdLineArgs.Contains("--multiLineCrack")) //multiLineCrack
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    for (int i = 0; i < 11; i++)
+                    {
+                        Console.WriteLine(StartString[i]);
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    foreach (string line in HashLine)
+                    {
+                        HashesToCrack.Add(line);
+                        Console.WriteLine($"Hashes2Crack: {line}");
+                    }
+
+                    foreach (string Hashes in HashesToCrack)
+                    {
+                        string tempHash = Hashes;
+                        var lines = File.ReadLines(wordlist);
+                        foreach (string Passline in lines)
+                        {
+                            //Console.WriteLine($"{MD5(Passline)}");
+                            if (MD5(Passline) == tempHash)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine($"Possible Password: {Passline} | HASH: {tempHash}");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                break;
+
+                            }
+                        }
+                        tempHash = string.Empty;
+                        //Thread.Sleep(1000);
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    for (int i = 0; i < 11; i++)
+                    {
+                        Console.WriteLine(StartString[i]);
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    foreach (string line in HashLine)
+                    {
+                        HashToCrack = line;
+                        Console.WriteLine($"Hash2Crack: {line}");
+                    }
+                    var lines = File.ReadLines(wordlist);
+                    foreach (string Passline in lines)
+                    {
+                        //Console.WriteLine($"{MD5(Passline)}");
+                        if (MD5(Passline) == HashToCrack)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Possible Password: {Passline} | HASH: {HashToCrack}");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+
+                        }
+                    }
+                }
+            }
+            
+            
+            
+        }
+
+        public static string MD5(string s)
+        {
+            var provider = System.Security.Cryptography.MD5.Create();
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+                builder.Append(b.ToString("x2").ToLower());
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ HashCrack/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HashCrack by [Obi/ProxNET/ProxFox]")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HashCrack")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("751291e0-501a-40e8-a6f8-855e7ea9875c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
